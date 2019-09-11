b38a26a96af79b484e9cd3e68afe4664cd2cb962
Łukasz Zaryczny <lukasz.zaryczny@gmail.com>
2019-09-11 15:20:03 +0200

    0.1.2.0 - First Build from starting ASP.NET webpage

--- CommunityMgmt/Properties/AssemblyInfo.cs
+++ CommunityMgmt/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("CommunityMgmt")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Community Management Database Inteface")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Dom na Skale")]
 [assembly: AssemblyProduct("CommunityMgmt")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright © Dom na Skale 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -29,7 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+// -----------------------------------------------------------------------------
+// Description: 0.1.2.0 - First Build from starting ASP.NET webpage
+[assembly: AssemblyVersion("0.1.2.0")]
+[assembly: AssemblyFileVersion("0.1.2.0")]
